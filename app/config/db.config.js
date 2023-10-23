dd02d15f3afcfee727285dafd4556d1f2401f9b5
charinu <charin.up@gmail.com>
2023-10-23 11:18:59 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+// module.exports = {
+//   HOST: "aws.connect.psdb.cloud",  //host on planet scale
+//   USER: "uol8c4md9olj06xvq1up",
+//   PASSWORD: "pscale_pw_BJVDaHUXJnrx3motCGY3JCaX8XXAXrMPKyFzwODCK0f",
+//   DB: "mydb"
+// };
---
+++ app/models/db.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+// const dbConfig = require("../config/db.config.js");
+
+require('dotenv').config()
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+
+// var connection = mysql.createPool({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB
+// });
+
+module.exports = connection;
